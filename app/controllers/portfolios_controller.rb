679b969f26e047568ab6154920e44ee7f9ca0b9a
Sooraj <sooraj@sooraj-mbp.local>
2017-07-29 17:48:56 +0200

    implemente new create edit update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,33 @@
 class PortfoliosController < ApplicationController
   def index
-    @portfilio_items=Portfolio.all
+    @portfolio_items=Portfolio.all
+  end
+  def new
+    @portfolio_items=Portfolio.new
+  end
+
+  def create
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item is successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  def edit
+    @portfolio_items=Portfolio.find(params[:id])
+  end
+  def update
+    @portfolio_items=Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_items.update(params.require(:portfolio).permit(:title, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item is successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 end
