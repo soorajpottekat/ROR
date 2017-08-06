b52a6f2e9c8a82b38948cb784917f9c96542644a
Sooraj <sooraj@sooraj-mbp.local>
2017-08-06 15:48:44 +0200

    Implemented custom scope for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170806083310) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
+    t.string "subtitle"
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
